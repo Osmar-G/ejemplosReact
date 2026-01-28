50dec4a4f4b12608d8c17c59dabd9beb35fa1d60
unknown <240036@utxicotepec.edu.mx>
2026-01-28 17:56:50 -0600

    Estilo al encabezado

--- nombre-app/src/Encabezado.jsx
+++ nombre-app/src/Encabezado.jsx
@@ -1,45 +1,55 @@
 import logo from "./assets/logo.png";
+import facebook from "./assets/facebook.png";
+import instagram from "./assets/instagram.png";
+import youtube from "./assets/youtube.png";
+import linkedin from "./assets/linkedln.png";
+import whatsapp from "./assets/whatsapp.png";
+import './encabezado.css';
+
 function Encabezado() {
-    return <div>
-        <Logotipo />
-        <Menu />
-        <Redes />
-    </div>
+    return (
+        <header className="encabezado">
+            <Logotipo />
+            <Menu />
+            <Redes />
+        </header>
+    );
 }
 
-function Logotipo(){
-    return(
-        <div>
+function Logotipo() {
+    return (
+        <div className="logo">
             <img src={logo} alt="Logotipo de la Aplicación" />
         </div>
-    )
+    );
 }
-function Menu(){
-    return(
-        <div>
-            
-                <ul>
-                    <li>Inicio</li>
-                    <li>Acerca de</li>
-                    <li>Productos</li>
-                    <li>Contacto</li>
-                    <li>Sucursales</li>
-                </ul>
-            
-        </div>
-    )
-}
-function Redes(){
-    return(
-        <div>
+
+function Menu() {
+    return (
+        <nav className="menu">
             <ul>
-                <li>Facebook</li>
-                <li>WhatsApp</li>
-                <li>Instagram</li>
-                <li>YouTube</li>
-                <li>LinkedIn</li>
+                <li>Inicio</li>
+                <li>Acerca de</li>
+                <li>Productos</li>
+                <li>Contacto</li>
+                <li>Sucursales</li>
+            </ul>
+        </nav>
+    );
+}
+
+function Redes() {
+    return (
+        <div className="redes">
+            <ul>
+                <li className="facebook"><img src={facebook} alt="Facebook" /></li>
+                <li className="whatsapp"><img src={whatsapp} alt="WhatsApp" /></li>
+                <li className="instagram"><img src={instagram} alt="Instagram" /></li>
+                <li className="youtube"><img src={youtube} alt="YouTube" /></li>
+                <li className="linkedin"><img src={linkedin} alt="LinkedIn" /></li>
             </ul>
         </div>
-    )
+    );
 }
+
 export default Encabezado;
