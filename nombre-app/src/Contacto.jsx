eec58bffd831ea5e4c67d7be0fc9b83a9f938b37
OsmarG <240036@utxicotepec.edu.mx>
2026-02-25 19:45:41 -0600

    Funcion de agregar usuarios

--- nombre-app/src/Contacto.jsx
+++ nombre-app/src/Contacto.jsx
@@ -1,16 +1,34 @@
+import React,{ useState} from "react";
+import api from "./Services/api.js";
 import './Contacto.css';
 
 function FormContacto() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const nuevoUsuario = {username, email, password};
+    try {
+        const respuesta = await api.post("/users", nuevoUsuario);
+        console.log("Usuario registrado:", respuesta.data);
+        alert('Usuario registrado exitosamente');
+    } catch (error) {
+        console.error("Error al registrar usuario:", error);
+    }
+}
   return (
     <div className="contacto-form">
       <h2>Contacto</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="nombre">Nombre</label>
         <input
           type="text"
           id="nombre"
           name="nombre"
-          placeholder="Escribe tu nombre"
+          placeholder="Escribe tu nombre de usuario"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
 
         <label htmlFor="correo">Correo</label>
@@ -19,15 +37,20 @@ function FormContacto() {
           id="correo"
           name="correo"
           placeholder="tuemail@ejemplo.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
-        <label htmlFor="mensaje">Mensaje</label>
-        <textarea
+        <label htmlFor="mensaje">Contraseña</label>
+        <input
+          type="password"
           id="mensaje"
           name="mensaje"
-          rows="4"
-          placeholder="Escribe tu mensaje aquí..."
-        ></textarea>
+          
+          placeholder="Escribe tu contraseña aquí..."
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
         <button type="submit">Enviar</button>
       </form>
